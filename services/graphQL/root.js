29258eedbddadfdf9eb3d3445d77e71e33e3865c
Maxime Burlat <maximeburlat@gmail.com>
2018-06-26 22:48:00 -0500

    first graphQL with Mongo

--- services/graphQL/root.js
+++ services/graphQL/root.js
@@ -1,10 +1,29 @@
-var tags = [
-  { id: "1", name: "premier", color: "blue" },
-  { id: "3", name: "second", color: "red" }
-];
+var db = require("./../models/index");
+
+class Tag {
+  constructor(id, { name, color }) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+  }
+}
 
 module.exports = {
   tag: function({ id }) {
-    return tags[id];
+    return db.Tag.findById(id).then(data => {
+      return new Tag(data._id.toString(), {
+        name: data.name,
+        color: data.color
+      });
+    });
+  },
+  createTag: function({ input }) {
+    console.log(input);
+    return db.Tag.create(input).then(data => {
+      return new Tag(data._id.toString(), {
+        name: data.name,
+        color: data.color
+      });
+    });
   }
 };
--- services/graphQL/schema.js
+++ services/graphQL/schema.js
@@ -3,11 +3,20 @@ var { buildSchema } = require("graphql");
 module.exports = buildSchema(`
   type Tag {
     id: ID!
-    name: String
+    name: String!
+    color: String
+  }
+
+  input TagInput {
+    name: String!
     color: String
   }
 
   type Query {
     tag(id: ID!): Tag
+  }
+
+  type Mutation {
+    createTag(input: TagInput): Tag
   }
 `);
